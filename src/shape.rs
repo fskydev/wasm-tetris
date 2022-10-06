4441c286f2532cf08237c27a646528edbc577085
fsky <falcon.skydiving.dev@gmail.com>
2022-10-06 12:03:59 -0700

    implement user interactions

--- src/shape.rs
+++ src/shape.rs
@@ -1,5 +1,4 @@
-use std::ops::Add;
-use std::collections::HashSet;
+use std::{collections::HashSet, ops::Add};
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 pub struct Pos(pub i32, pub i32);
@@ -27,20 +26,11 @@ macro_rules! impl_shape_constructor {
           anchor: $anchor,
         }
       }
-    )*    
+    )*
   };
 }
 
 impl Shape {
-  // pub fn new_i() -> Self {
-  //   Self {
-  //     positions: [Pos(0, 0), Pos(1, 0), Pos(2, 0), Pos(3, 0)]
-  //       .into_iter()
-  //       .collect(),
-  //     anchor: Pos(1, 0),
-  //   }
-  // }
-
   impl_shape_constructor! {
     new_i: [Pos(0, 0), Pos(1, 0), Pos(2, 0), Pos(3, 0)] anchored at Pos(1, 0);
     new_o: [Pos(0, 0), Pos(1, 0), Pos(0, 1), Pos(1, 1)] anchored at Pos(0, 0);
@@ -64,6 +54,14 @@ impl Shape {
       6 => Self::new_z(),
       _ => unreachable!(),
     }
+  }
+
+  pub fn positions(&self) -> impl Iterator<Item = Pos> + '_ {       // bound to the self lifetime
+    self.positions.iter().copied()
+  }
+
+  pub fn collides_with(&self, other: &Shape) -> bool {
+    self.positions.intersection(&other.positions).count() > 0
   }
 }
 
--- src/tetris.rs
+++ src/tetris.rs
@@ -1,9 +1,16 @@
 use crate::shape::{Pos, Shape};
+use std::mem;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Direction {
+  Left,
+  Right,
+}
 
 #[derive(Debug)]
 pub struct Tetris {
-  width: u32,
-  height: u32,
+  width: i32,
+  height: i32,
   current_shape: Shape,
   fixed_shapes: Vec<Shape>,
 }
@@ -11,15 +18,53 @@ pub struct Tetris {
 impl Tetris {
   pub fn new(width: u32, height: u32) -> Self {
     Self {
-      width,
-      height,
-      current_shape: &Shape::new_random() + Pos((width as i32) / 2, 0),           //todo!(),
+      width: width as i32,
+      height: height as i32,
+      current_shape: &Shape::new_random() + Pos((width as i32) / 2, 0), //todo!(),
       fixed_shapes: vec![],
     }
   }
 
+  pub fn is_out_of_bounds(&self, shape: &Shape) -> bool {
+    !shape
+      .positions()
+      .all(|pos| 0 <= pos.0 && pos.0 < self.width && 0 <= pos.1 && pos.1 < self.height)
+  }
+
+  pub fn is_colliding(&self, shape: &Shape) -> bool {
+    self
+      .fixed_shapes
+      .iter()
+      .any(|fixed_shape| fixed_shape.collides_with(shape))
+  }
+
   pub fn tick(&mut self) {
-    
+    let translated_current_shape = &self.current_shape + Pos(0, 1);
+
+    if self.is_out_of_bounds(&translated_current_shape)
+      || self.is_colliding(&translated_current_shape)
+    {
+      let new_fixed_shape = mem::replace(
+        &mut self.current_shape,
+        &Shape::new_random() + Pos(self.width / 2, 0),
+      );
+
+      self.fixed_shapes.push(new_fixed_shape);
+    } else {
+      self.current_shape = translated_current_shape;
+    }
+  }
+
+  pub fn shift(&mut self, direction: Direction) {
+    let translated_current_shape = &self.current_shape
+      + match direction {
+        Direction::Left => Pos(-1, 0),
+        Direction::Right => Pos(1, 0),
+      };
+
+    if !self.is_out_of_bounds(&translated_current_shape) && !self.is_colliding(&translated_current_shape) {
+      self.current_shape = translated_current_shape;
+    }
   }
 }
 
@@ -29,6 +74,11 @@ mod tests {
 
   #[test]
   fn test() {
-    println!("{:#?}", Tetris::new(10, 30));
+    let mut tetris = Tetris::new(10, 30);
+    tetris.tick();
+    tetris.tick();
+    tetris.tick();
+
+    println!("{:#?}", tetris);
   }
 }
